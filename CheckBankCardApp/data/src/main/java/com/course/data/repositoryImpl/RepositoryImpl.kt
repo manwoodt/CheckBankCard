7ca85532513e5b90bc7d7ba928b750d916e75891
manwoodtWindows <manwoodt@gmail.com>
2025-02-05 17:52:41 +0300

    remake RepositoryImpl, add func about binInfoHistory

--- CheckBankCardApp/data/src/main/java/com/course/data/repositoryImpl/RepositoryImpl.kt
+++ CheckBankCardApp/data/src/main/java/com/course/data/repositoryImpl/RepositoryImpl.kt
@@ -1,13 +1,35 @@
 package com.course.data.repositoryImpl
 
 import com.course.data.api.BinApiService
+import com.course.data.mappers.toDomain
+import com.course.data.mappers.toEntity
+import com.course.data.room.BinInfoDao
+import com.course.domain.model.BinHistoryItem
 import com.course.domain.model.BinInfo
 import com.course.domain.repository.BinRepository
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.map
 
-class BinRepositoryImpl(private val apiService: BinApiService) : BinRepository {
+class BinRepositoryImpl(
+    private val apiService: BinApiService,
+    private val dao: BinInfoDao,
+) : BinRepository {
 
-    override suspend fun getBinInfo(binNumber: String): BinInfo {
-        return apiService.getBinInfo(binNumber)
+    override suspend fun getBinInfo(bin: String): BinInfo {
+        val response = apiService.getBinInfo(bin)
+        dao.insertBinInfo(response.toEntity(bin))
+        return response
     }
 
+    override suspend fun getBinInfoHistory()
+            : Flow<List<BinHistoryItem>> {
+        return dao.getAllBinInfo().map { entities ->
+            entities.map { it.toDomain() }
+        }
+    }
+
+    override suspend fun clearHistory() {
+       dao.deleteAll()
+    }
 }
+
